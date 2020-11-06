98901cb4b5db61ba3467f940db36ba95a6046f2b
Bradley Dion <bradleydion@bradleys-mbp.lan>
2020-11-06 16:01:06 -0500

    adding anything i didnt before shutting down my computer

--- user-onboarding/cypress/integration/examples/form_test.js
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -7,8 +7,8 @@ describe('Form test', ()=>{
     })
 
     const nameInput = ()=> cy.get('input[name = name');
-    const emailInput = () => cy.get('input [name = email]');
-    const passwordInput = () => cy.get('input [name = password]');
+    const emailInput = () => cy.get('input[name = email]');
+    const passwordInput = () => cy.get('input[name = password]');
     
     describe ('Testing Name Input',()=>{
         it('should fill in the name input',()=>{
@@ -23,6 +23,13 @@ describe('Form test', ()=>{
             emailInput().should('have.value',"")
             .type('thisemail@somewhere.com')
             .should('have.value','thisemail@somewhere.com')
+        })
+    })
+    describe('testing password input field',()=>{
+        it('should fill in the password',()=>{
+            passwordInput().should('have.value',"")
+            .type('*******')
+            .should('have.value','*******')
         })
     })
 })
